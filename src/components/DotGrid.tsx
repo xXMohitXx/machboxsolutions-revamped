21eefb71688e891e4e8e519b77b29e2162b3c3a7
Mohit Manglani <68803303+xXMohitXx@users.noreply.github.com>
2025-12-12 19:13:45 +0530

    done

--- src/components/DotGrid.tsx
+++ src/components/DotGrid.tsx
@@ -48,7 +48,7 @@ function hexToRgb(hex: string) {
 }
 const DotGrid: React.FC<DotGridProps> = ({
   dotSize = 16,
-  gap = 32,
+  gap = 40,
   baseColor = '#5227FF',
   activeColor = '#5227FF',
   proximity = 150,
